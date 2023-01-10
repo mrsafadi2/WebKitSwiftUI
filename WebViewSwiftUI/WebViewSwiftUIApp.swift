9966c9c460ae75288f8dd24f45abf684aaf842c2
mrsafadi2 <mrsafadi2@gmail.com>
2023-01-10 20:31:19 +0200

    Initial Commit

---
+++ WebViewSwiftUI/WebViewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewSwiftUIApp.swift
+//  WebViewSwiftUI
+//
+//  Created by Mohammed Safadi Macbook Pro on 10/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct WebViewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
